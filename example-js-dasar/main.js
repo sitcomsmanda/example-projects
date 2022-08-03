481f1b31ed12ff2ae6c9c300fb767046307ff2a9
DikDns <dikdnssocial@gmail.com>
2022-08-03 20:43:36 +0700

    add: js dasar project

---
+++ example-js-dasar/main.js
@@ -0,0 +1,66 @@
+function romanToInt(str) {
+  function charToInt(c) {
+    switch (c) {
+      case "I":
+        return 1;
+      case "V":
+        return 5;
+      case "X":
+        return 10;
+      case "L":
+        return 50;
+      case "C":
+        return 100;
+      case "D":
+        return 500;
+      case "M":
+        return 1000;
+      default:
+        return -1;
+    }
+  }
+
+  if (str == null) return -1;
+
+  let num = charToInt(str.charAt(0));
+  let pre, curr;
+
+  for (let i = 1; i < str.length; i++) {
+    curr = charToInt(str.charAt(i));
+    pre = charToInt(str.charAt(i - 1));
+    if (curr <= pre) {
+      num += curr;
+    } else {
+      num = num - pre * 2 + curr;
+    }
+  }
+
+  return num;
+}
+
+function intToRoman(num) {
+  const lookup = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let roman = "";
+  let i;
+  for (i in lookup) {
+    while (num >= lookup[i]) {
+      roman += i;
+      num -= lookup[i];
+    }
+  }
+  return roman;
+}
